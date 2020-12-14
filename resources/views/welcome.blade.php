c5885ecce352939fc62bc6eeb6f2f4c698fb18d3
Roman <roman82direct@yandex.ru>
2020-12-14 16:27:26 +0300

    add base template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,111 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>Laravel</title>
+@section('title', 'Page Title')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+@section('menu')
+    @parent
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+@endsection
 
-            .full-height {
-                height: 100vh;
-            }
+@section('content')
+    <p class="great">{{$message}}</p>
+@endsection
 
-            .flex-center {
-                /*align-items: center;*/
-                display: flex;
-                justify-content: center;
-            }
+{{--<!DOCTYPE html>--}}
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
+{{--    <head>--}}
+{{--        <meta charset="utf-8">--}}
+{{--        <meta name="viewport" content="width=device-width, initial-scale=1">--}}
 
-            .position-ref {
-                position: relative;
-            }
+{{--        <title>Laravel</title>--}}
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+{{--        <!-- Fonts -->--}}
+{{--        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">--}}
 
-            .content {
-                text-align: center;
-            }
+{{--        <!-- Styles -->--}}
+{{--        <style>--}}
+{{--            html, body {--}}
+{{--                background-color: #fff;--}}
+{{--                color: #636b6f;--}}
+{{--                font-family: 'Nunito', sans-serif;--}}
+{{--                font-weight: 200;--}}
+{{--                height: 100vh;--}}
+{{--                margin: 0;--}}
+{{--            }--}}
 
-            .title {
-                font-size: 84px;
-            }
+{{--            .full-height {--}}
+{{--                height: 100vh;--}}
+{{--            }--}}
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+{{--            .flex-center {--}}
+{{--                /*align-items: center;*/--}}
+{{--                display: flex;--}}
+{{--                justify-content: center;--}}
+{{--            }--}}
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+{{--            .position-ref {--}}
+{{--                position: relative;--}}
+{{--            }--}}
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+{{--            .top-right {--}}
+{{--                position: absolute;--}}
+{{--                right: 10px;--}}
+{{--                top: 18px;--}}
+{{--            }--}}
 
-            <div class="content">
-                <div class="links">
-                    <a href="/public">Главная</a>
-                    <a href="/public/categories">Категории новостей</a>
-                    <a href="/public/signin">Авторизация</a>
-                    <a href="/public/admin">Админка</a>
-                </div>
-                <br>
+{{--            .content {--}}
+{{--                text-align: center;--}}
+{{--            }--}}
 
-                <div class="title m-b-md">
-                    <p>{{$message}}</p>
-                </div>
+{{--            .title {--}}
+{{--                font-size: 84px;--}}
+{{--            }--}}
 
-            </div>
-        </div>
-    </body>
-</html>
+{{--            .links > a {--}}
+{{--                color: #636b6f;--}}
+{{--                padding: 0 25px;--}}
+{{--                font-size: 13px;--}}
+{{--                font-weight: 600;--}}
+{{--                letter-spacing: .1rem;--}}
+{{--                text-decoration: none;--}}
+{{--                text-transform: uppercase;--}}
+{{--            }--}}
+
+{{--            .m-b-md {--}}
+{{--                margin-bottom: 30px;--}}
+{{--            }--}}
+{{--        </style>--}}
+{{--    </head>--}}
+{{--    <body>--}}
+{{--        <div class="flex-center position-ref full-height">--}}
+{{--            @if (Route::has('login'))--}}
+{{--                <div class="top-right links">--}}
+{{--                    @auth--}}
+{{--                        <a href="{{ url('/home') }}">Home</a>--}}
+{{--                    @else--}}
+{{--                        <a href="{{ route('login') }}">Login</a>--}}
+
+{{--                        @if (Route::has('register'))--}}
+{{--                            <a href="{{ route('register') }}">Register</a>--}}
+{{--                        @endif--}}
+{{--                    @endauth--}}
+{{--                </div>--}}
+{{--            @endif--}}
+
+{{--            <div class="content">--}}
+{{--                <div class="links">--}}
+{{--                    <a href="/public">Главная</a>--}}
+{{--                    <a href="/public/categories">Категории новостей</a>--}}
+{{--                    <a href="/public/signin">Авторизация</a>--}}
+{{--                    <a href="/public/admin">Админка</a>--}}
+{{--                </div>--}}
+{{--                <br>--}}
+
+{{--                <div class="title m-b-md">--}}
+{{--                    <p>{{$message}}</p>--}}
+{{--                </div>--}}
+
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </body>--}}
+{{--</html>--}}
